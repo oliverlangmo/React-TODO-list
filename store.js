ccdffb9b0dd9e29ae8a91847a9f4c26c51bfc225
oliverlangmo <oliver.langmo@gmail.com>
2016-08-19 15:12:17 -0500

    first commit

---
+++ store.js
@@ -0,0 +1,70 @@
+var items = [];
+
+var notifyComponents = function() {
+  $(ListStore).trigger('storeHasChanged');
+};
+
+var findItemById = function(id) {
+  return items.filter(function(item) {
+    return item.id === id;
+  })[0];
+},
+
+ListStore = {
+
+  getItems: function() {
+    return items;
+  },
+
+  loadItems: function() {
+  var loadRequest = $.ajax({
+    type: 'GET',
+    url: "https://listalous.herokuapp.com/lists/oliverlangmolist/"
+  });
+  loadRequest.done(function(dataFromServer){
+    items = dataFromServer.items;
+    notifyComponents();
+  });
+  },
+  addItem: function(itemDescription) {
+   var creationRequest = $.ajax({
+     type:'POST',
+     url: "http://listalous.herokuapp.com/lists/oliverlangmolist/items",
+     data: {description: itemDescription, completed: false}
+   });
+   creationRequest.done(function(itemDataFromServer){
+     items.push(itemDataFromServer);
+     notifyComponents();
+   });
+
+  },
+toggleCompleteness: function(itemId) {
+var item = findItemById(itemId);
+var currentCompletedValue = item.completed;
+
+var updateRequest = $.ajax({
+  type: 'PUT',
+  url: "https://listalous.herokuapp.com/lists/oliverlangmolist/items/" + itemId,
+  data: { completed: !currentCompletedValue }
+});
+updateRequest.done(function(itemData) {
+  item.completed = itemData.completed;
+  notifyComponents();
+});
+},
+deleteMe: function(itemId){
+  var item = findItemById(itemId);
+  var currentItemtoDelete = item.deleted;
+
+  var removeItem = $.ajax({
+    type: "DELETE",
+    url: "https://listalous.herokuapp.com/lists/oliverlangmolist/items/" + itemId,
+    data: {deleted: !currentItemtoDelete}
+  });
+  removeItem.done(function(itemData){
+    item.deleted = itemData.deleted;
+    notifyComponents();
+  });
+}
+
+};
